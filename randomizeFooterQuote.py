7a0ccce18e28edb094fbe7eafca225d3cd97d95b
Daniel Tan <danieltan.singapore@gmail.com>
2022-09-19 20:54:14 +0800

    Added more quotes primarily on experimentation

--- randomizeFooterQuote.py
+++ randomizeFooterQuote.py
@@ -11,7 +11,16 @@ quotes = [
     "The statistician cannot evade the responsibility for understanding the process he applies or recommends - Ronald Fisher",
     "Sucking at something is the 1st step to being sorta good at something - Jake the Dog",
     "Reading furnishes the mind only with materials of knowledge. It is thinking that makes what we read ours - John Locke",
-    "Die trying - Van Neistat"
+    "Die trying - Van Neistat",
+    "The purpose of visualization is insight, not pictures - Ben Shneiderman",
+    "We try to solve very complicated problems without letting people know how complicated the problem was - Johnathan Ives",
+    "There is no such thing as informational overload, just bad design. If something is cluttered or confusing, fix your design - Edward Tufte",
+    "You don't understand anything until you learn it more than one way - Marvin Minsky",
+    "Somewhere, something incredible is waiting to be known - Carl Sagan",
+    "I am convinced the best learning takes place when the leaner takes charge - Seymour Papert",
+    "We must never make experiments to confirm our ideas, but simply to control them - Claude Bernard",
+    "If your experiment needs statistics, you ought to have done a better experiment - Ernest Rutherford",
+    "Study hard what interests you the most in the most undisciplined, irreverent and original manner possible - Richard Feynman",
     ]
 randomQuote = random.choice(quotes)
 
